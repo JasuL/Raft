50b846723d87cb4185d42fe9c7f38b28fa42a6a4
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2015-04-28 01:43:37 -0400

    nearly have elections working

--- src/edu/duke/raft/CandidateMode.java
+++ src/edu/duke/raft/CandidateMode.java
@@ -1,59 +1,106 @@
 package edu.duke.raft;
 
+import java.util.Timer;
 public class CandidateMode extends RaftMode {
-  public void go () {
-    synchronized (mLock) {
-      int term = 0;      
-      System.out.println ("S" + 
-			  mID + 
-			  "." + 
-			  term + 
-			  ": switched to candidate mode.");
-    }
-  }
+	private Timer myCountVotesTimer;
+	private int COUNT_VOTES_TIMER_ID = 1;
+	private Timer myElectionTimeoutTimer;
+	private int ELECTION_TIMEOUT_TIMER_ID = 2;
+	private int COUNT_VOTES_INTERVAL = 25;
+	private int ELECTION_TIME_LENGTH = 100;
+	public void go () {
+		synchronized (mLock) {
+			this.incrementTerm();    
+			System.out.println ("S" + 
+					mID + 
+					"." + 
+					mConfig.getCurrentTerm() + 
+					": switched to candidate mode.");
+		}
+		this.beginElection();
 
-  // @param candidate’s term
-  // @param candidate requesting vote
-  // @param index of candidate’s last log entry
-  // @param term of candidate’s last log entry
-  // @return 0, if server votes for candidate; otherwise, server's
-  // current term 
-  public int requestVote (int candidateTerm,
-			  int candidateID,
-			  int lastLogIndex,
-			  int lastLogTerm) {
-    synchronized (mLock) {
-      int term = mConfig.getCurrentTerm ();
-      int result = term;
-      return result;
-    }
-  }
-  
+	}
+	private void incrementTerm(){
+		mConfig.setCurrentTerm(mConfig.getCurrentTerm()+1,0);
+	}
+	private void beginElection(){
+		//Set a timer for checking for responses.
+		//Set a timer for ending election.
+		myCountVotesTimer = scheduleTimer(this.COUNT_VOTES_INTERVAL,this.COUNT_VOTES_TIMER_ID); 
+		myElectionTimeoutTimer = scheduleTimer(this.ELECTION_TIME_LENGTH,this.ELECTION_TIMEOUT_TIMER_ID);
+		for(int i=1;i<=mConfig.NUM_SERVERS;i++){
+			this.remoteRequestVote(i, mConfig.getCurrentTerm(), mID, mLastApplied, mConfig.getCurrentTerm()-1); //is this right??
+		}
+	}
 
-  // @param leader’s term
-  // @param current leader
-  // @param index of log entry before entries to append
-  // @param term of log entry before entries to append
-  // @param entries to append (in order of 0 to append.length-1)
-  // @param index of highest committed entry
-  // @return 0, if server appended entries; otherwise, server's
-  // current term
-  public int appendEntries (int leaderTerm,
-			    int leaderID,
-			    int prevLogIndex,
-			    int prevLogTerm,
-			    Entry[] entries,
-			    int leaderCommit) {
-    synchronized (mLock) {
-      int term = mConfig.getCurrentTerm ();
-      int result = term;
-      return result;
-    }
-  }
+	// @param candidate’s term
+	// @param candidate requesting vote
+	// @param index of candidate’s last log entry
+	// @param term of candidate’s last log entry
+	// @return 0, if server votes for candidate; otherwise, server's
+	// current term 
+	public int requestVote (int candidateTerm,
+			int candidateID,
+			int lastLogIndex,
+			int lastLogTerm) {
+		synchronized (mLock) {
+			
+			System.out.println("Vote requested from candidate server " + mID);	
+//			int term = mConfig.getCurrentTerm ();
+//			int result = term;
+//			return result;
+			return 0;
+		}
+	}
 
-  // @param id of the timer that timed out
-  public void handleTimeout (int timerID) {
-    synchronized (mLock) {
-    }
-  }
+
+	// @param leader’s term
+	// @param current leader
+	// @param index of log entry before entries to append
+	// @param term of log entry before entries to append
+	// @param entries to append (in order of 0 to append.length-1)
+	// @param index of highest committed entry
+	// @return 0, if server appended entries; otherwise, server's
+	// current term
+	public int appendEntries (int leaderTerm,
+			int leaderID,
+			int prevLogIndex,
+			int prevLogTerm,
+			Entry[] entries,
+			int leaderCommit) {
+		synchronized (mLock) {
+			int term = mConfig.getCurrentTerm ();
+			int result = term;
+			return result;
+		}
+	}
+
+	// @param id of the timer that timed out
+	//ID=1 Count Votes
+	//ID=2 Election has timed out... call a second election.
+	public void handleTimeout (int timerID) {
+		synchronized (mLock) {
+			if(timerID==this.ELECTION_TIMEOUT_TIMER_ID){
+				this.myCountVotesTimer.cancel();
+				this.myElectionTimeoutTimer.cancel();
+				this.incrementTerm();
+				this.beginElection();
+			}
+			if(timerID==this.COUNT_VOTES_TIMER_ID){
+				//count the votes
+				int[] votes = RaftResponses.getVotes(mConfig.getCurrentTerm());
+				int voteCounter=0;
+				for(int i=0;i<votes.length;i++){
+					if(votes[i]==0){
+						voteCounter++;
+					}
+				}
+				if(voteCounter>votes.length/2){
+					this.myCountVotesTimer.cancel();
+					this.myElectionTimeoutTimer.cancel();
+					RaftServerImpl.setMode(new LeaderMode());
+				}
+			}
+		}
+	}
 }
--- src/edu/duke/raft/FollowerMode.java
+++ src/edu/duke/raft/FollowerMode.java
@@ -1,60 +1,78 @@
 package edu.duke.raft;
 
+import java.util.Random;
+import java.util.Timer;
 public class FollowerMode extends RaftMode {
-  public void go () {
-    synchronized (mLock) {
-      int term = 0;
-      System.out.println ("S" + 
-			  mID + 
-			  "." + 
-			  term + 
-			  ": switched to follower mode.");
-    }
-  }
-  
-  // @param candidate’s term
-  // @param candidate requesting vote
-  // @param index of candidate’s last log entry
-  // @param term of candidate’s last log entry
-  // @return 0, if server votes for candidate; otherwise, server's
-  // current term
-  public int requestVote (int candidateTerm,
-			  int candidateID,
-			  int lastLogIndex,
-			  int lastLogTerm) {
-    synchronized (mLock) {
-      int term = mConfig.getCurrentTerm ();
-      int vote = term;
-      return vote;
-    }
-  }
-  
+	private Timer myTimer;
+	public void go () {
+		synchronized (mLock) {
+			int term = 0;
+			System.out.println ("S" + 
+					mID + 
+					"." + 
+					term + 
+					": switched to follower mode.");
+			Random rand = new Random();
+			myTimer = scheduleTimer(rand.nextInt(this.ELECTION_TIMEOUT_MAX - this.ELECTION_TIMEOUT_MIN) + this.ELECTION_TIMEOUT_MIN, this.mID); //may need to change timer id here
 
-  // @param leader’s term
-  // @param current leader
-  // @param index of log entry before entries to append
-  // @param term of log entry before entries to append
-  // @param entries to append (in order of 0 to append.length-1)
-  // @param index of highest committed entry
-  // @return 0, if server appended entries; otherwise, server's
-  // current term
-  public int appendEntries (int leaderTerm,
-			    int leaderID,
-			    int prevLogIndex,
-			    int prevLogTerm,
-			    Entry[] entries,
-			    int leaderCommit) {
-    synchronized (mLock) {
-      int term = mConfig.getCurrentTerm ();
-      int result = term;
-      return result;
-    }
-  }  
+		}
+	}
 
-  // @param id of the timer that timed out
-  public void handleTimeout (int timerID) {
-    synchronized (mLock) {
-    }
-  }
+	// @param candidate’s term
+	// @param candidate requesting vote
+	// @param index of candidate’s last log entry
+	// @param term of candidate’s last log entry
+	// @return 0, if server votes for candidate; otherwise, server's
+	// current term
+	public int requestVote (int candidateTerm,
+			int candidateID,
+			int lastLogIndex,
+			int lastLogTerm) {
+		synchronized (mLock) {
+			myTimer.cancel(); //Not sure if we should cancel the timer here
+			System.out.println("Vote requested from follower server " + mID);
+			//Vote for candidate if it has at least as up to date term
+			//Vote for candidate if it has at least as up to date entries (TODO)
+			int term = mConfig.getCurrentTerm ();
+			int vote = term;
+			if(lastLogTerm>=term){
+				//Additional checks to be added for log status
+				System.out.println("Follower " + mID + " voting for candidate " + candidateID);
+				return 0;
+			}
+			return vote;
+		}
+	}
+
+
+	// @param leader’s term
+	// @param current leader
+	// @param index of log entry before entries to append
+	// @param term of log entry before entries to append
+	// @param entries to append (in order of 0 to append.length-1)
+	// @param index of highest committed entry
+	// @return 0, if server appended entries; otherwise, server's
+	// current term
+	public int appendEntries (int leaderTerm,
+			int leaderID,
+			int prevLogIndex,
+			int prevLogTerm,
+			Entry[] entries,
+			int leaderCommit) {
+		synchronized (mLock) {
+			int term = mConfig.getCurrentTerm ();
+			int result = term;
+			return result;
+		}
+	}  
+
+	// @param id of the timer that timed out
+	public void handleTimeout (int timerID) {
+		synchronized (mLock) {
+			myTimer.cancel();
+			System.out.println(timerID + " has detected the leader has TIMED OUT\n");
+			RaftServerImpl.setMode(new CandidateMode()); //Not exactly sure how this line works...
+		}
+	}
 }
 
